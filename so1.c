8f1fffa5811e800d8b3cbfd5476c1e01555dfc1c
Hotaru <37961000+heyhotaru@users.noreply.github.com>
2022-04-29 21:38:22 -0300

    Add files via upload

--- so1.c
+++ so1.c
@@ -3,71 +3,90 @@
 #include <locale.h>
 
 
-int main(){
-	int n1, n2;
-	
+int main()
+{
+
 	system("cls");
 	system("COLOR 0A");
-	
-	
+
+
 	setlocale(LC_ALL, "Portuguese");
-	
-	printf("\nOlá, infomática\n");
+
+	printf("\tOlá, infomática\n");
 	Sleep(2000);
-	
-	printf("\nINICIAR SISTEMA...\n");
+	printf("\tINICIAR SISTEMA...\n");
 	Sleep(2000);
-	
-	printf("\nChecagem de memoria RAM\n");
+	printf("\tChecagem de memoria RAM\n");
 	Sleep(2000);
-	
-	printf("..........\n");
-	
+	printf("\t..........\n");
 	Sleep(2000);
- 
- 
+
+
 	Sleep(5000);
-	system("cls");	
-	printf("Carregando 1% ");Sleep(500);system("cls");
-	printf("Carregando 2% ");Sleep(500);system("cls");
-	printf("Carregando 3% ");Sleep(500);system("cls");
-	printf("Carregando 4% ");Sleep(500);system("cls");
-	printf("Carregando 5% ");Sleep(500);system("cls");
-		
- printf("#     #\n");
- printf("#     #  ####  #####   ##   #####  #    #\n");
- printf("#     # #    #   #    #  #  #    # #    #\n");
- printf("####### #    #   #   #    # #    # #    #\n");
- printf("#     # #    #   #   ###### #####  #    #\n");
- printf("#     # #    #   #   #    # #   #  #    #\n");
- printf("#     #  ####    #   #    # #    #  ####  \n");
+	system("cls");
+	printf("Carregando 1% ");
+	Sleep(500);
+	system("cls");
+	printf("Carregando 2% ");
+	Sleep(500);
+	system("cls");
+	printf("Carregando 3% ");
+	Sleep(500);
+	system("cls");
+	printf("Carregando 4% ");
+	Sleep(500);
+	system("cls");
+	printf("Carregando 5% ");
+	Sleep(500);
+	system("cls");
 
- 
-system("COLOR 2F");
-printf("DIGITE UMA OPÇÃO DE BOOT: \n");
-printf("\n \t1 - USB\n \t2 - HDD\n \t3 - CD-ROM\n");
-int opcao;
-scanf("%i",&opcao);
+	printf("#     #\n");
+	printf("#     #  ####  #####   ##   #####  #    #\n");
+	printf("#     # #    #   #    #  #  #    # #    #\n");
+	printf("####### #    #   #   #    # #    # #    #\n");
+	printf("#     # #    #   #   ###### #####  #    #\n");
+	printf("#     # #    #   #   #    # #   #  #    #\n");
+	printf("#     #  ####    #   #    # #    #  ####  \n");
 
-if(opcao == 1){
-	printf("Iniciando sistema via USB\n");Sleep(3000);
-} 
 
-else if(opcao == 2){
-	printf("Iniciando sistema via Disco Rigido");Sleep(3000);	
-}
+	system("COLOR 2F");
+	printf("DIGITE UMA OPÇÃO DE BOOT: \n");
+	printf("\n \t1 - USB\n \t2 - HDD\n \t3 - CD-ROM\n");
+	int opcao;
+	scanf("%i", &opcao);
 
-else if(opcao == 3){
-	printf("Iniciando sistema via CD-ROM");Sleep(3000);	
-}
+	switch(opcao)
+	{
 
-else{
-	printf("OPÇÃO INVÁLIDA!!!");Sleep(3000);
-}
- 
 
- 
-                                            
+	case 1:
+		printf("\tIniciando sistema via USB\n");
+		Sleep(3000);
+		break;
+
+	case 2:
+		printf("\tIniciando sistema via Disco Rigido\n");
+		Sleep(3000);
+		break;
+
+
+	case 3:
+		printf("\tIniciando sistema via CD-ROM\n");
+		Sleep(3000);
+		break;
+
+
+	default:
+		printf("\tOpção invalida\n");
+		printf("\tReniciando o sistema\n");
+		break;
+
+
+	}
+
+
+
+
 	return 0;
-	
+
 }
